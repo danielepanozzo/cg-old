7e2709162af4e0b0b6b66da2baf49138273f471f
Daniele Panozzo <daniele.panozzo@gmail.com>
2016-10-22 22:40:37 -0400

    * added blending example * updated slides

---
+++ Assignment_2/extra/main_blending.cpp
@@ -0,0 +1,193 @@
+// This example is heavily based on the tutorial at https://open.gl
+
+// OpenGL Helpers to reduce the clutter
+#include "Helpers.h"
+
+// GLFW is necessary to handle the OpenGL context
+#include <GLFW/glfw3.h>
+
+// Linear Algebra Library
+#include <Eigen/Core>
+
+// Timer
+#include <chrono>
+
+// VertexBufferObject wrapper
+VertexBufferObject VBO;
+
+// Contains the vertex positions
+Eigen::MatrixXf V;
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    // Get the position of the mouse in the window
+    double xpos, ypos;
+    glfwGetCursorPos(window, &xpos, &ypos);
+
+    // Get the size of the window
+    int width, height;
+    glfwGetWindowSize(window, &width, &height);
+
+    // Convert screen position to world coordinates
+    double xworld = ((xpos/double(width))*2)-1;
+    double yworld = (((height-1-ypos)/double(height))*2)-1; // NOTE: y axis is flipped in glfw
+
+    // Update the position of the first vertex if the left button is pressed
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+        V.col(0) << xworld, yworld;
+
+    // Upload the change to the GPU
+    VBO.update(V);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    // Update the position of the first vertex if the keys 1,2, or 3 are pressed
+    switch (key)
+    {
+        case  GLFW_KEY_1:
+            V.col(0) << -0.5,  0.5;
+            break;
+        case GLFW_KEY_2:
+            V.col(0) << 0,  0.5;
+            break;
+        case  GLFW_KEY_3:
+            V.col(0) << 0.5,  0.5;
+            break;
+        default:
+            break;
+    }
+
+    // Upload the change to the GPU
+    VBO.update(V);
+}
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    // Initialize the library
+    if (!glfwInit())
+        return -1;
+
+    // Activate supersampling
+    glfwWindowHint(GLFW_SAMPLES, 8);
+
+    // Ensure that we get at least a 3.2 context
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+
+    // On apple we have to load a core profile with forward compatibility
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // Create a windowed mode window and its OpenGL context
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    // Initialize the VAO
+    // A Vertex Array Object (or VAO) is an object that describes how the vertex
+    // attributes are stored in a Vertex Buffer Object (or VBO). This means that
+    // the VAO is not the actual object storing the vertex data,
+    // but the descriptor of the vertex data.
+    VertexArrayObject VAO;
+    VAO.init();
+    VAO.bind();
+
+    // Initialize the VBO with the vertices data
+    // A VBO is a data container that lives in the GPU memory
+    VBO.init();
+
+    V.resize(2,6);
+    V <<
+    0,  0.5, -0.5, 0.1,  0.6, -0.4,
+    0.5, -0.5, -0.5, 0.6, -0.4, -0.4;
+    VBO.update(V);
+
+    // Initialize the OpenGL Program
+    // A program controls the OpenGL pipeline and it must contains
+    // at least a vertex shader and a fragment shader to be valid
+    Program program;
+    const GLchar* vertex_shader =
+            "#version 150 core\n"
+                    "in vec2 position;"
+                    "void main()"
+                    "{"
+                    "    gl_Position = vec4(position, 0.0, 1.0);"
+                    "}";
+    const GLchar* fragment_shader =
+            "#version 150 core\n"
+                    "out vec4 outColor;"
+                    "uniform vec4 triangleColor;"
+                    "void main()"
+                    "{"
+                    "    outColor = vec4(triangleColor);"
+                    "}";
+
+    // Compile the two shaders and upload the binary to the GPU
+    // Note that we have to explicitly specify that the output "slot" called outColor
+    // is the one that we want in the fragment buffer (and thus on screen)
+    program.init(vertex_shader,fragment_shader,"outColor");
+    program.bind();
+
+    // The vertex shader wants the position of the vertices as an input.
+    // The following line connects the VBO we defined above with the position "slot"
+    // in the vertex shader
+    program.bindVertexAttribArray("position",VBO);
+
+    // Register the keyboard callback
+    glfwSetKeyCallback(window, key_callback);
+
+    // Register the mouse callback
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
+
+    // Loop until the user closes the window
+    while (!glfwWindowShouldClose(window))
+    {
+        // Bind your VAO (not necessary if you have only one)
+        VAO.bind();
+
+        // Bind your program
+        program.bind();
+
+        // Clear the framebuffer
+        glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Enable blending test
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+        // Draw a triangle (red)
+        glUniform4f(program.uniform("triangleColor"), 1.0f, 0.0f, 0.0f, 1.0f);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // Draw a triangle (green)
+        glUniform4f(program.uniform("triangleColor"), 0.0f, 1.0f, 0.0f, 0.5f);
+        glDrawArrays(GL_TRIANGLES, 3, 3);
+
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+
+        // Poll for and process events
+        glfwPollEvents();
+    }
+
+    // Deallocate opengl memory
+    program.free();
+    VAO.free();
+    VBO.free();
+
+    // Deallocate glfw internals
+    glfwTerminate();
+    return 0;
+}
